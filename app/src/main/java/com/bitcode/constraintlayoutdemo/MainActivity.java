34e9c21cfbd82887aefbf226e0ca06583e0efdc5
aishwarya-phatak <ashp17star@gmail.com>
2022-08-11 12:25:58 +0530

    Radio Buttons as a part of Radio Group in constraint layout.

---
+++ app/src/main/java/com/bitcode/constraintlayoutdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.bitcode.constraintlayoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"From is Submitted",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
